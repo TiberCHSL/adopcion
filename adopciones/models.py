fd394ad7818c606fce78905bbbc923b771bc382d
TiberCHSL <tibercarlitohsl@gmail.com>
2023-12-29 14:37:27 -0300

    Avanzando

--- adopciones/models.py
+++ adopciones/models.py
@@ -24,11 +24,18 @@ class Organizacion(models.Model):
     correo = models.CharField(max_length=50, verbose_name = "Correo de contacto de la organizacion", default = "correodefault@gmail.com")
     contacto = models.CharField(max_length=25,verbose_name = 'Telefono de contacto de la organizacion', default = "+569")
 
+    def __str__(self):
+        return self.nombre_org
+
 class SedeOrganizacion(models.Model):
     nombre_org = models.ForeignKey(Organizacion, on_delete=models.CASCADE)
+    nombre_sede = models.CharField(max_length = 50, verbose_name = "Nombre de la sede", default = "Nombre por defecto")
     comuna = models.ForeignKey(Comuna, on_delete=models.CASCADE)
     direccion = models.CharField(max_length = 100, verbose_name = "Direccion de la sede")
     contacto = models.CharField(max_length=25,verbose_name = 'Telefono de contacto de la sede', default = "+569")
+    
+    def __str__(self):
+        return self.nombre_sede
 
 class Usuario(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,verbose_name = "Nombre de usuario")
@@ -38,9 +45,32 @@ class Usuario(models.Model):
     gender = models.CharField(max_length=10, verbose_name="Género", choices=[('M', 'Masculino'), ('F', 'Femenino'), ('O', 'Otro')])
     role = models.CharField(max_length=10, verbose_name="Rol de usuario", choices=[('A', 'Adoptante'), ('O', 'Organizacion')], default= 'A')
     comuna = models.ForeignKey(Comuna, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.user
+
+
+class DatosPagoUsuario(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,verbose_name = "Nombre de usuario")
+    banco = models.CharField(max_length=50)
+    acc_type = models.CharField(max_length=50, choices=[('C','Cuenta corriente'),('V','Cuenta vista'),('A','Cuenta de ahorro')])
+    num_cuenta = models.CharField(max_length=50,verbose_name = "Numero de la cuenta")
+    def __str__(self):
+        return self.user
+
+
+class Pagos(models.Model):
+    num_pago = models.ForeignKey(DatosPagoUsuario,on_delete = models.CASCADE, verbose_name = 'numero del pago')
+    monto = models.IntegerField(verbose_name = 'Cantidad')
+    def __str__(self):
+        return self.num_pago
+
 
 class TipoMascota(models.Model):
     tipo_nombre = models.CharField(primary_key = True, max_length=10, verbose_name="Tipo de mascota", choices=[('P', 'Perro'), ('G', 'Gato'), ('O', 'Otro')])
+    
+    def __str__(self):
+        return self.tipo_nombre
+
 
 class Mascota(models.Model):
     id_sede_org = models.ForeignKey(SedeOrganizacion, max_length=12, verbose_name="Sede de la organizacion" , on_delete=models.CASCADE, default = "0")
@@ -52,11 +82,20 @@ class Mascota(models.Model):
     soc_3 = models.CharField(max_length=25, verbose_name="Sociable con gatos", choices=[('S', 'Si'), ('N', 'No'), ('N/A', 'No se sabe')], default ='N/A' )
     rut = models.ForeignKey(Usuario, max_length=12, verbose_name="RUT" , on_delete=models.CASCADE, blank = True, null = True)
     edad_est = models.IntegerField(verbose_name = "Edad estimada de la mascota")
+    estado_adopcion = models.BooleanField(default = False)
+    
+    def __str__(self):
+        return str(self.tipo_nombre)
+
     #Revisar
 
 class Imagen(models.Model):
     img_mascota = models.ImageField(upload_to="Img_mascotas", verbose_name= "Imagen de la mascota")
     id_mascota = models.ForeignKey(Mascota, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.id_mascota)
+
     #Averiguar como heredar ID default
 
 class Seguimiento(models.Model):
@@ -86,11 +125,13 @@ class Colecta(models.Model):
     nombre_org = models.ForeignKey(Organizacion, max_length=12, verbose_name="Nombre de la organizacion" , on_delete=models.CASCADE)
     nombre_colecta = models.CharField(max_length = 50, verbose_name = "Nombre de la colecta")
     descripcion_colecta = models.TextField(verbose_name = 'Descripcion de la colecta')
-    rut = models.CharField(max_length=12, verbose_name="RUT de la cuenta")
     banco = models.CharField(max_length=50)
     acc_type = models.CharField(max_length=50, choices=[('C','Cuenta corriente'),('V','Cuenta vista'),('A','Cuenta de ahorro')])
     num_cuenta = models.CharField(max_length=50,verbose_name = "Numero de la cuenta")
     monto_inicial = models.IntegerField(verbose_name = "Monto inicial de colecta (0)")
+    
+    def __str__(self):
+        return self.nombre_colecta
 
 
 
--- adopciones/views.py
+++ adopciones/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Mascota, Imagen
 
 def index(request):
-    return render(request, "index.html")
+    mascotas = Mascota.objects.select_related('id_sede_org__comuna__region').all()
+
+    imagenes_mascotas = {mascota.id: Imagen.objects.filter(id_mascota=mascota.id).first() for mascota in mascotas}
+
+    return render(request, 'index.html', {'mascotas': mascotas, 'imagenes_mascotas': imagenes_mascotas})
